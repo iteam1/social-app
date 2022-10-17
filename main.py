ae188c513d1f309fdcf499c10bed3b90c9986e89
locchuong <locchuong123@gmail.com>
2022-10-18 03:53:53 +0700

    add posts-path

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/") # method and path
 def root(): # the function option async
 	return{"message":"wellcome to my api"} # FastAPI auto convert dict into json
+
+# add another path operation
+@app.get("/posts")
+def get_posts():
+	return {"data":"This is your posts"}
