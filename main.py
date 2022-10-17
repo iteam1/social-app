b969bb22045a1d6b20e96e944e21dbba9a55b301
locchuong <locchuong123@gmail.com>
2022-10-18 04:00:38 +0700

    update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 # init app
 app = FastAPI()
 
+# look for the first match
+# when you send a request to a path, FastAPI starting search from the top and stop at path where your path is match
 # decorator
 @app.get("/") # method and path
 def root(): # the function option async
