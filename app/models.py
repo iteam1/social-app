e11f85f48203776b39597dabf61010f00940f6a8
locchuong <locchuong123@gmail.com>
2022-10-21 12:59:00 +0700

    add timestamp column

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column,Integer,String,Boolean
-from sqlalchemy.sql.expression import null
+from sqlalchemy.sql.expression import null,text
+from sqlalchemy.sql.sqltypes import TIMESTAMP
 from .database import Base
 
 # this is python model also databas table, Changing setup in here you need to delete the table
@@ -9,4 +10,5 @@ class Post(Base): # Capitialize
 	id = Column(Integer,primary_key = True,nullable = False)
 	title = Column(String,nullable = False)
 	content = Column(String,nullable=False)
-	published = Column(Boolean,server_default = 'TRUE') 
+	published = Column(Boolean,server_default = 'TRUE')
+	create_at = Column(TIMESTAMP(timezone=True),nullable =False,server_default= text('now()'))
