ab99e77759edef1bd31e614dfb3002ff4583688e
locchuong <locchuong123@gmail.com>
2022-10-23 02:27:00 +0700

    add group api

--- app/routers/post.py
+++ app/routers/post.py
@@ -7,14 +7,14 @@ from .. import models
 from ..database import get_db
 from ..schemas import PostBase,PostCreate,PostUpdate,PostResponse
 
-router = APIRouter() # create router object
+router = APIRouter(prefix = '/post',tags = ['Posts']) # create router object
 
-@router.get("/posts",response_model = List[PostResponse]) # Pydantic format
+@router.get("/",response_model = List[PostResponse]) # Pydantic format
 def get_posts(db:Session= Depends(get_db)): # Pydantic format
 	posts = db.query(models.Post)
 	return posts.all() #{"data":posts.all()}
 
-@router.post("/posts",status_code = status.HTTP_201_CREATED,response_model = PostResponse) # Pydantic format
+@router.post("/",status_code = status.HTTP_201_CREATED,response_model = PostResponse) # Pydantic format
 def create_post(post:PostCreate,db:Session = Depends(get_db)): # Pydantic format
 	new_post = models.Post(**post.dict()) # unpackage form 
 	db.add(new_post) # add new row
@@ -22,7 +22,7 @@ def create_post(post:PostCreate,db:Session = Depends(get_db)): # Pydantic format
 	db.refresh(new_post)
 	return new_post# Follow PostResponse 
 
-@router.get("/posts/{id}",response_model = PostResponse)
+@router.get("/{id}",response_model = PostResponse)
 def get_post(id: int,db: Session = Depends(get_db)): # str as default
 	post_query = db.query(models.Post).filter(models.Post.id == id)
 	post = post_query.first()
@@ -31,7 +31,7 @@ def get_post(id: int,db: Session = Depends(get_db)): # str as default
 							detail = f"post with id {id} not found")
 	return post #{"post_detail": post}
 
-@router.delete("/posts/{id}",status_code = status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}",status_code = status.HTTP_204_NO_CONTENT)
 def delete_post(id:int,db:Session = Depends(get_db)):
 	post_query = db.query(models.Post).filter(models.Post.id==id)
 	post = post_query.first()
@@ -46,7 +46,7 @@ def delete_post(id:int,db:Session = Depends(get_db)):
 
 	return Response(status_code = status.HTTP_204_NO_CONTENT)
 
-@router.put("/posts/{id}",response_model = PostResponse)
+@router.put("/{id}",response_model = PostResponse)
 def update_post(id:int,update_post:PostBase,db:Session = Depends(get_db)): #,data:dict=Body(...) must be in the last of declaration
 	
 	post_query = db.query(models.Post).filter(models.Post.id == id)
@@ -67,7 +67,7 @@ def update_post(id:int,update_post:PostBase,db:Session = Depends(get_db)): #,dat
 
 	return post_query.first() #{"updated_post": post_query.first(),"message":"post updated!"}
 
-@router.patch("/posts/{id}",response_model = PostResponse)
+@router.patch("/{id}",response_model = PostResponse)
 def update_field(id:int,response:Response,data:dict=Body(...),db: Session= Depends(get_db)): #,data:dict=Body(...) must be in the last of declaration
 	
 	post_query = db.query(models.Post).filter(models.Post.id == id)
--- app/routers/user.py
+++ app/routers/user.py
@@ -8,14 +8,14 @@ from ..database import get_db
 from ..schemas import UserCreate,UserOut
 from ..utils import hash_pass
 
-router = APIRouter()
+router = APIRouter(prefix = '/users', tags = ['Users'])
 
-@router.get("/users",response_model = List[UserOut])
+@router.get("/",response_model = List[UserOut])
 def get_users(db: Session = Depends(get_db)):
 	users = db.query(models.User)
 	return users.all()
 
-@router.get("/users/{id}",response_model = List[UserOut])
+@router.get("/{id}",response_model = List[UserOut])
 def get_user(id:int,db:Session = Depends(get_db)):
 	user  = db.query(models.User).filter(models.User.id == id).first()
 	if not user:
@@ -23,9 +23,15 @@ def get_user(id:int,db:Session = Depends(get_db)):
 							detail = f"user with id {id} not found")
 	return user
 
-@router.post("/users",status_code = status.HTTP_201_CREATED,response_model = UserOut)
+@router.post("/",status_code = status.HTTP_201_CREATED,response_model = UserOut)
 def create_user(user: UserCreate ,db: Session = Depends(get_db)):
 
+	# validate the email
+	current_user = db.query(models.User).filter(models.User.email == user['email']).first()
+	if current_user:
+		raise HTTPException(status_code = status.HTTP_404_CONFLICT,
+							detail = f"{user.email} is already be taken")
+		
 	#hash the password -user.password
 	hashed_password = hash_pass(user.password)
 	user.password = hashed_password
