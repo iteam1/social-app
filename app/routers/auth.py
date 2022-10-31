2286b255ca2ec0d61a640d207854e4b54d2e1f25
locchuong <locchuong123@gmail.com>
2022-11-01 05:20:58 +0700

    done login

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -14,16 +14,6 @@ router = APIRouter(prefix = '/login',tags= ['authentication'])
 @router.post('/',response_model = Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(),db :Session= Depends(get_db)): #user_credentials: UserLogin
 
-	# OAuthPasswordRequestForm is gonna return a dict content but it only required to give username and password
-	# {
-	# "username":"...",
-	# "password":"...",
-	# "scope":"...",
-	# "client_id":"...",
-	# "client_sercet":"...",
-	# "grant_type":"...",
-	# }
-
 	user = db.query(models.User).filter(models.User.email== user_credentials.username).first()
 	
 	if not user:
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,22 @@
 import pytest
 from app import schemas
+from app.config import settings
 from .database import client,session
+from jose import jwt
+
+@pytest.fixture(scope = 'function')
+def test_user(client):
+	print('creating new user')
+	data = {
+		"email":"tester2@email.com",
+		"password": "123"
+	}
+	res = client.post("/users/",json = data)
+	assert res.status_code == 201
+	print(res.json())
+	new_user = res.json()
+	new_user['password'] = data['password'] # add field password
+	return new_user
 
 def test_create_user(client):
 	# send data in body, effect to database
@@ -8,18 +24,26 @@ def test_create_user(client):
 		"email":"tester@email.com",
 		"password": "123"
 	}
-	res = client.post('/users/',json = data )
+	res = client.post('/users/',json = data)
 	print(res.json())
 	new_user = schemas.UserOut(**res.json())
 	assert new_user.email  == data['email']
 	assert res.status_code == 201
 
-def test_login_user(client):
-	data = {
-		"username":"tester@email.com",
-		"password": "123"
-	}
-	res = client.post('/login/',data = data ) # from data
-	print(res.json())
-
+def test_login_user(client,test_user):
+	res = client.post('/login/',data = {
+		"username": test_user['email'],
+		"password": test_user['password']
+	})
+	# check status code
 	assert res.status_code == 200
+	print(res.json())
+	login_res = schemas.Token(**res.json()) # pydantic object
+	payload = jwt.decode(login_res.access_token,settings.secret_key,algorithms=[settings.algorithm])
+	# check the token type
+	token_type = login_res.token_type
+	assert token_type == "bearer"
+	# check id of user match
+	id = payload.get('user_id')
+	assert id == test_user['id']
+
